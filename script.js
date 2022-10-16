dd02fe0e7a5e8f83308524474ab3139833cd50a2
Isaac Kim <kisaac95@yahoo.com>
2022-10-16 16:32:33 -0700

    bit more progress for questions

---
+++ script.js
@@ -0,0 +1,36 @@
+// window.alert('Hello')
+// window.alert("Let us play a game!")
+// window.alert("wait that sounds creepy, not on some Saw shit this is just a simple trivia game project. nothing bad will happen I promise.")
+// let consent = window.prompt("If you win, there is a sizable cash prize. Wanna play?"
+// if(consent != 'yes' && "Yes" && "YES") {
+//     window.alert("bummer...:(")
+// }
+// document.body.querySelector('h2')
+
+function Reset(){
+    let reset= document.nav.ul.li.getElementsByClassName('reset')
+    reset.addEventListener('click', function(){
+        window.location.reload();
+    })
+}
+Reset()
+
+// question list
+// waht is the capital of Burkina Faso? (Ouagadougou)
+// who is the first person to win two Nobel Prizes? (Marie Curie)
+// What country did the United States invade following the September 11th attacks? (Afghanistan)
+// how much is a human life worth? (any/every answer correct)
+// What is the Earliest literate civilization in human history? (Mesopotamia)
+// What one piece character said "Justice will prevail you say? But of course it will, Whoever wins (this war) becomes justice"? (Donquixote Doflamingo)
+// Are you a good person? (No)
+// Is it better to live as a kind and rightous slave to wretched men, or a cruel and callous deviant against those same men?
+// what is the square root of 987654321? (31426.9680529)
+// Do you ignore any evils?
+// Are you better than others? 
+// Is one responsible for the sins of those you support?
+// where in the human body does oxygenation of the blood occur? (Alveoli)
+// Who is the patron deity of the United States(money)
+// Is your life worth it? What of those that you know suffer greater? 
+// Isnt it easy to rationalize excuses?
+// Are you a good being? 
+// Of course you think so. 
