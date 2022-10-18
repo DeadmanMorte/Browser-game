6e939cac7dd05d83b4a0354a1cb84a75961246e0
Isaac Kim <kisaac95@yahoo.com>
2022-10-17 22:43:25 -0700

    added functions frameworks and background usic

--- script.js
+++ script.js
@@ -7,15 +7,55 @@
 // }
 // document.body.querySelector('h2')
 
+
+let element
+const score = 0 
+
 function Reset(){
-    let reset= document.nav.ul.li.getElementsByClassName('reset')
+    let reset = document.nav.ul.getElementsByClassName('reset')
     reset.addEventListener('click', function(){
         window.location.reload();
     })
 }
-Reset()
 
-// question list
+// function runaway(){
+//     let reset= document.nav.ul.li.getElementsByClassName('Runaway')
+//     reset.addEventListener('click', function(){
+//       let element = document.body.createElement('h1')
+//       element.body.innerHTML.append('You cant just save and quit from reality. you cant pause this. Time does not care for your schedule or desires at all. Nor do I.')
+//       .then
+//       window.prompt('Will you run away?')
+//         if yes then window.location.reload()/ or crash the site
+//     })
+// }
+
+// function Forfeit(){
+//     let reset= document.nav.ul.li.getElementsByClassName('surrender')
+//     reset.addEventListener('click', function(){
+//         element.body.innerHTML.append('I do not blame you. The burdens of existence are often too much to bear. If this is you true and honest choice I will not patronize you. Are you certain you would like to end it all?')
+//     })
+// }
+// Reset()
+
+// function updateScore(){
+//     if button pressed = correct
+//     document.body.button.style.backgroundColor = 'green'
+//     score = score + 1
+//     let scoreBoard = document.body.getElementsByClassName('score')
+//     scoreBoard.innerHTML.append(score)
+    
+//     return score
+// }
+
+// function newQuestion(){
+//     document.body.button.style.backgroundColor = 'white'
+//     element = document.body.getElementsByTagName('h3')
+//     element.innerHTML.append(question[]++)
+// }
+
+
+
+// question list (array?)
 // waht is the capital of Burkina Faso? (Ouagadougou)
 // who is the first person to win two Nobel Prizes? (Marie Curie)
 // What country did the United States invade following the September 11th attacks? (Afghanistan)
