02938a6410d54910a5f924972c5edda16c8398a9
Isaac Kim <kisaac95@yahoo.com>
2022-10-20 21:33:03 -0700

    added mas functionality, dynamic buttons and prompts

--- script.js
+++ script.js
@@ -7,50 +7,28 @@
 // }
 // document.body.querySelector('h2')
 
-
+var correctBtn
 let element
+var i = 0
 const score = 0 
-var buttonsClicked = [false, false]
+var buttonsClicked = [false, false, false, false]
+var question = questions[i]
 
  function buttonClicked(button){
     buttonsClicked[button] = true;
-    question1()
-}
-
-function Reset(){
-    element = document.getElementsByClassName("reset");
-    element[0].addEventListener('click', function(){
-        window.location.reload();
-    })
+    validateQuestion(button)
 }
 
 
-function question1(){
-    if(buttonsClicked[0] == true){
-        document.getElementsByClassName("btn1")[0].style.backgroundColor = 'green'
-        
-    } else if (buttonsClicked[1] == true){
-        document.getElementsByClassName("btn2")[0].style.backgroundColor = 'red'
-    }
+function validateQuestion(button){
+    if(buttonsClicked[button] == true && button == correctBtn){
+        document.getElementsByClassName('responseBtns')[0].style.backgroundColor = 'green'
+        console.log('booty')
+    } else 
+        document.getElementsByClassName('responseBtns')[0].style.backgroundColor = 'red'
+        console.log('titty')
+    // newQuestion()
 }
-
-// function runaway(){
-//     element = document.getElementsByClassName('Runaway');
-//     element[0].addEventListener('click', function(){
-//       let run = document.createElement('h1');
-//         run[0].innerHTML = 'You cant just save and quit from reality. You do not get to pause this. Time does not care for your schedule or desires at all. Nor do I.'
-//             document.body.container.append(run)
-//     })
-// }
-// runaway()
-
-// function Forfeit(){
-//     let reset= document.nav.ul.li.getElementsByClassName('surrender')
-//     reset.addEventListener('click', function(){
-//         element.body.innerHTML.append('I do not blame you. The burdens of existence are often too much to bear. If this is you true and honest choice I will not patronize you. Are you certain you would like to end it all?')
-//     })
-// }
-// Reset()
 
 // function updateScore(){
 //     if button pressed = correct
@@ -62,31 +40,88 @@ function question1(){
 //     return score
 // }
 
-// function newQuestion(){
-//     document.body.button.style.backgroundColor = 'white'
-//     element = document.body.getElementsByTagName('h3')
-//     element.innerHTML.append(question[]++)
-// }
+function newQuestion(){
+    document.getElementsByTagName('h3')[0].textContent = question.text;
+    getRandomButton();
+    return correctBtn
+    i++;
+}
 
-// questions = [
-// waht is the capital of Burkina Faso? (Ouagadougou)
-// who is the first person to win two Nobel Prizes? (Marie Curie)
-// What country did the United States invade following the September 11th attacks? (Afghanistan)
-// how much is a human life worth? (any/every answer correct)
-// What is the Earliest literate civilization in human history? (Mesopotamia)
-// What one piece character said "Justice will prevail you say? But of course it will, Whoever wins (this war) becomes justice"? (Donquixote Doflamingo)
-// Are you a good person? (No)
-// Is it better to live as a kind and rightous slave to wretched men, or a cruel and callous deviant against those same men?
-// what is the square root of 987654321? (31426.9680529)
-// Do you ignore any evils?
-// Are you better than others? 
-// Is one responsible for the sins of those you support?
-// where in the human body does oxygenation of the blood occur? (Alveoli)
-// Who is the patron deity of the United States(money)
-// Is your life worth it? What of those that you know suffer greater? 
-// Isnt it easy to rationalize excuses?
-// Are you a good being? 
-// Of course you think so. ]
+ function getRandomButton() {
+  let num = Math.floor(Math.random() * 4);
+  switch(num) {
+    case 0:
+        document.getElementsByClassName('btn1')[0].innerHTML = question.answer;
+                document.getElementsByClassName('btn2')[0].innerHTML = question.op1;
+                    document.getElementsByClassName('btn3')[0].innerHTML = question.op2;
+                        document.getElementsByClassName('btn4')[0].innerHTML = question.op3;
+                            correctBtn = 0;
 
+        break;
+
+    case 1: 
+        document.getElementsByClassName('btn1')[0].innerHTML = question.op1;  
+            document.getElementsByClassName('btn2')[0].innerHTML = question.answer;
+                document.getElementsByClassName('btn3')[0].innerHTML = question.op2;
+                    document.getElementsByClassName('btn4')[0].innerHTML = question.op3;
+                        correctBtn = 1;
+        break;
+  
+    case 2:       
+        document.getElementsByClassName('btn1')[0].innerHTML = question.op1;
+            document.getElementsByClassName('btn2')[0].innerHTML = question.op2;
+                document.getElementsByClassName('btn3')[0].innerHTML = question.answer;
+                    document.getElementsByClassName('btn4')[0].innerHTML = question.op3;
+                        correctBtn = 2;
+        break;
+  
+    case 3:
+            document.getElementsByClassName('btn1')[0].innerHTML = question.op1;
+                document.getElementsByClassName('btn3')[0].innerHTML = question.op2;
+                    document.getElementsByClassName('btn2')[0].innerHTML = question.op3;
+                        document.getElementsByClassName('btn4')[0].innerHTML = question.answer;
+                            correctBtn = 3;
+  }
+  return correctBtn
+ }
+
+ function Reset(){
+    element = document.getElementsByClassName("reset");
+    element[0].addEventListener('click', function(){
+        window.location.reload();
+    })
+}
+
+function runaway(){
+    element = document.getElementsByClassName('Runaway');
+    element[0].addEventListener('click', function(){
+      window.alert('You cant just save and quit from reality. You do not get to pause this. Time does not care for your schedule or desires at all. Nor do I.')
+            
+    })
+}
+
+function Forfeit(){
+    element = document.getElementsByClassName('surrender');
+    element[0].addEventListener('click', function(){
+        document.getElementsByTagName('h3')[0].textContent = 'I do not blame you. The burdens of existence are often too much to bear. If this is your true and honest choice I will not patronize you. Are you certain you would like to end it all?';
+            document.getElementsByClassName('btn1')[0].innerHTML = 'Yes';
+                document.getElementsByClassName('btn2')[0].innerHTML = 'No';
+                    document.getElementsByClassName('btn3')[0].innerHTML = "heck no";
+                        document.getElementsByClassName('btn4')[0].innerHTML = "mama aint raise no punk"
+    })
+}
+
+function forfeitChoice(){
+    if(buttonsClicked[0] == true){
+        document.getElementsByClassName("btn1")[0].style.backgroundColor = 'red';
+        // perhaps crash the site?
+    } if( buttonsClicked[1,2,3] == true){
+        window.alert('nice');
+        window.alert('I know it can get hard, but the struggle to be is noble. You are noble. Never doubt that, so long as you try.');
+        newQuestion()
+    }
+}
+newQuestion()
 Reset()
-question1()
+runaway()
+Forfeit()
